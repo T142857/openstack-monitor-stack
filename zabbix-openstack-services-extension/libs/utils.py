e2f09f3f5ed37806be2d771a05b0910e764c9d22
jokerbui <toan.chev@gmail.com>
2018-07-26 19:05:58 +0700

    add comment

--- zabbix-openstack-services-extension/libs/utils.py
+++ zabbix-openstack-services-extension/libs/utils.py
@@ -1,3 +1,22 @@
+"""
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+**
+** Author: Joker Bui <toan.chev@gmail.com>
+**
+** Version: 1.0 Last update: 2018-07-26 12:00
+"""
+
 import os
 import sys
 import json
